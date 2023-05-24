449581aedb0ac88c5fd48319d67b78c5605d1b1d
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-24 20:11:18 -0300

    setup

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        box-sizing: border-box;
+    }
+    button {
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        background-color: #a328d6;
+        font-size: 20px;
+        font-weight: 600;
+        color: #fff;
+        cursor: pointer;
+        width: 100%;
+        padding: 12px;
+    }
+`
+
+export default GlobalStyle
