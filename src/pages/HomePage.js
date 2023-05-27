941114c035cd2ef5570bea9888656f00cc8d50d8
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-27 10:20:45 -0300

    start

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,13 @@
+import styled from "styled-components"
+
 export default function HomePage(){
     return(
-        <>
-        </>
+        <MainDiv>
+            oi
+        </MainDiv>
     )
 }
+
+const MainDiv=styled.div`
+    background-color: #2596be;
+`
